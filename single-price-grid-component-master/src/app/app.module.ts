c58f16d79a0ddf7c20cfe5974e3cf61e4334d1ca
JoseaneGrigolo <joseaneig@gmail.com>
2021-06-04 15:41:12 -0300

    salvando antes q esqueca

---
+++ single-price-grid-component-master/src/app/app.module.ts
@@ -0,0 +1,23 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { BodyComponent } from './body/body.component';
+import { ButtonComponent } from './button/button.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+    BodyComponent,
+    ButtonComponent
+
+  ],
+  imports: [
+    BrowserModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
